51939ddba8f85384a1eecb3f5c7087932292be88
Mateus <mateus.rosengartenn@gmail.com>
2026-02-26 16:06:16 -0300

    fix:delete dotenv for deploy

--- src/server/dbInstance/index.ts
+++ src/server/dbInstance/index.ts
@@ -7,15 +7,12 @@ declare global {
 const pool =
   global.pgPool ||
   new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: Number(process.env.DB_PORT),
-    ssl:
-      process.env.NODE_ENV === 'production'
-        ? { rejectUnauthorized: false }
-        : false,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: Number(process.env.POSTGRES_PORT),
+    ssl: { rejectUnauthorized: false },
   });
 
 if (process.env.NODE_ENV !== 'production') {
